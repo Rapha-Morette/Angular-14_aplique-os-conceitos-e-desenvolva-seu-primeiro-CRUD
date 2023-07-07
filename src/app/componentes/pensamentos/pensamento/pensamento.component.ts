dd1697ddc332cf4790d396c425a5a1f23fbaca31
Rapha-Morette <raphaelmrtt14@gmail.com>
2023-07-07 16:21:21 -0300

    Módulo 4  concluído

--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -3,13 +3,19 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-pensamento',
   templateUrl: './pensamento.component.html',
-  styleUrls: ['./pensamento.component.css']
+  styleUrls: ['./pensamento.component.css'],
 })
 export class PensamentoComponent {
   @Input() pensamento = {
     conteudo: 'I love Angular',
     autoria: 'Nay',
-    modelo: 'modelo3'
-  }
+    modelo: 'modelo3',
+  };
 
+  larguraPensamento(): string {
+    if (this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g';
+    }
+    return 'pensamento-p';
+  }
 }
