d1afaa576cabe3e116defc5b2c513e6dc625646d
Rapha-Morette <raphaelmrtt14@gmail.com>
2023-07-12 15:54:22 -0300

    Módulo 6 video 2 concluído

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 import { Pensamento } from './pensamento';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PensamentoService {
+  private readonly API = 'http://localhost:3000/pensamentos';
+  constructor(private http: HttpClient) {}
 
-  private readonly API = 'http://localhost:3000/pensamentos'
-  constructor(private http: HttpClient) { }
-
-  listar() {
+  listar(): Observable<Pensamento[]> {
     return this.http.get<Pensamento[]>(this.API);
   }
 
+  criar(pensamento: Pensamento): Observable<Pensamento> {
+    return this.http.post<Pensamento>(this.API, pensamento);
+  }
 }
